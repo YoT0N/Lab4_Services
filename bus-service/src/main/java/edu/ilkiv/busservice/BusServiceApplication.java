a6850b8c657e5ba37e8b0b531fc31cb6d64a284b
YoT0N <145184198+YoT0N@users.noreply.github.com>
2025-05-28 11:11:32 +0300

    Add files via upload

--- bus-service/src/main/java/edu/ilkiv/busservice/BusServiceApplication.java
+++ bus-service/src/main/java/edu/ilkiv/busservice/BusServiceApplication.java
@@ -2,8 +2,10 @@ package edu.ilkiv.busservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BusServiceApplication {
 
     public static void main(String[] args) {
